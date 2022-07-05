119c509c48ba062314c9ed403d765223c2949bf8
Muhammad Eizlan <eizs798@gmail.com>
2022-07-05 09:26:23 +0800

    Feat | add function claimWC

--- hera/custom.js
+++ hera/custom.js
@@ -1,37 +1,53 @@
 function copyToClipboard() {
-    document.getElementById("invite-link").select();
-    document.execCommand('copy');
+  document.getElementById("invite-link").select();
+  document.execCommand("copy");
 }
-var masterUrl =  $("#master_url").val();
+var masterUrl = $("#master_url").val();
 
 function claimPC(data) {
-    let id = data.getAttribute("data-face");
-    $.ajax({
-        url: masterUrl+"/points/claim-requests" + '/' + id + "",
-        type: "GET",
-        dataType: "JSON",
-        success: function(data) {
-            $('#points-value').text('Points: '+data.remainder);
-        	alert("Claim request sent! sit back relax..");
-        },
-        error : function() {
-            alert("Something's wrong :(");
-        }
-    });
+  let id = data.getAttribute("data-face");
+  $.ajax({
+    url: masterUrl + "/points/claim-requests" + "/" + id + "",
+    type: "GET",
+    dataType: "JSON",
+    success: function (data) {
+      $("#points-value").text("Points: " + data.remainder);
+      alert("Claim request sent! sit back relax..");
+    },
+    error: function () {
+      alert("Something's wrong :(");
+    },
+  });
 }
 
 function claimCC(data) {
-    let id = data.getAttribute("data-face");
-    $.ajax({
-        url: masterUrl+"/cashclaim/claim-requests" + '/' + id + "",
-        type: "GET",
-        dataType: "JSON",
-        success: function(data) {
-            $('#cashclaim-value').text('Cash : RM 0');
-        	alert("Claim request sent! sit back relax..");
-        },
-        error : function() {
-            alert("Something's wrong :(");
-        }
-    });
+  let id = data.getAttribute("data-face");
+  $.ajax({
+    url: masterUrl + "/cashclaim/claim-requests" + "/" + id + "",
+    type: "GET",
+    dataType: "JSON",
+    success: function (data) {
+      $("#cashclaim-value").text("Cash : RM 0");
+      alert("Claim request sent! sit back relax..");
+    },
+    error: function () {
+      alert("Something's wrong :(");
+    },
+  });
+}
+
+function claimWC(data) {
+  let id = data.getAttribute("data-face");
+  $.ajax({
+    url: masterUrl + "/walletclaim/claim-requests" + "/" + id + "",
+    type: "GET",
+    dataType: "JSON",
+    success: function (data) {
+      $("#walletclaim-value").text("Wallet : RM 0");
+      alert("Claim request sent! sit back relax..");
+    },
+    error: function () {
+      alert("Something's wrong :(");
+    },
+  });
 }
